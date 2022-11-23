fff02a9465a1225d7080b94fa67e2031738f814b
marcin <marcin@digger.dev>
2022-11-23 07:46:57 +0100

    Install dgctl from pip on linux

--- dgctl-npm/install.js
+++ dgctl-npm/install.js
@@ -8,39 +8,62 @@ function getPlatform() {
 
     if (platform == 'darwin') {
         return 'osx';
-    } else if (platform == 'win32') {
-        return 'windows'
-    } else { 
-        console.error(`platform ${platform} not supported .. exiting`)
-        process.exit()
     }
+    if (platform == 'win32') {
+        return 'windows';
+    }
+    if (platform == 'linux') {
+        return 'linux';
+    }
+
+    console.error(`platform ${platform} not supported... exiting`)
+    process.exit()
 }
 
-async function main() {
-    var homedir = require('os').homedir()
-    var platform = getPlatform()
+async function downloadBinaryFromGithub(platform) {
     var downloadUrl = `https://github.com/diggerhq/dgctl/releases/download/latest-release/dgctl-${platform}.zip`;
+    var zipFile = "dgctl.zip";
 
     console.log(`Fetching ${downloadUrl}`)
 
     await axios.get(downloadUrl, {
       responseType: 'stream',
     }).then((response) => {
-      response.data.pipe(fs.createWriteStream("dgctl.zip"))
+      response.data.pipe(fs.createWriteStream(zipFile))
         .on('close', function() {
-          fs.createReadStream("dgctl.zip").pipe(unzipper.Extract({path: "."}))
+          fs.createReadStream(zipFile).pipe(unzipper.Extract({path: "."}))
             .on('close', function() {
+              fs.unlinkSync(zipFile);
               if (platform == "osx") {
                 child_process.execSync(`chmod +x dgctl`)
-                console.log("Well done! Run ./dgctl");
+                console.log("Well done! You can run './dgctl' now");
               }
 
               if (platform == "windows") {
-                console.log("Well done! Run dgctl.exe");
+                console.log("Well done! You can run 'dgctl.exe' now");
               }
             });
           });
     });
+
+}
+
+async function installFromPip() {
+    console.log(`Installing dgctl from Python Package Repository`)
+    child_process.execSync(`python -m pip install dgctl`)
+}
+
+async function main() {
+    var homedir = require('os').homedir()
+    var platform = getPlatform()
+
+    if (platform == "linux") {
+        await installFromPip();
+        console.log("Well done! You can run 'dgctl' now");
+        return;
+    }
+
+    await downloadBinaryFromGithub(platform);
 }
 
 main()
